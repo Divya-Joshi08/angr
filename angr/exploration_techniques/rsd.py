56df1c0cd96645a29538e6f214562ffa626fba48
Divya-Joshi08 <joshidivya08@gmail.com>
2025-03-07 16:33:48 -0600

    rsd exp tech

---
+++ angr/exploration_techniques/rsd.py
@@ -0,0 +1,20 @@
+from __future__ import annotations
+
+from .base import ExplorationTechnique
+
+'''
+class RSD(ExplorationTechnique):
+
+setup():
+    make the static control dependence graph
+
+step():
+    i think we can use their implementation
+
+selector():
+    maybe we can use this to not step redundant states ? 
+
+Questions:
+- when is the dynamic control dependence graph constructed?
+'''
+
