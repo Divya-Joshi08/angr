9968ecd116796295b03c3d8615a507c12cae19db
Temitayo Awosemo <temia@utexas.edu>
2025-04-16 04:41:30 -0500

    silly control dependency implementation. probably wrong. also not tested.

--- angr/analyses/dynamic_dependency_graph.py
+++ angr/analyses/dynamic_dependency_graph.py
@@ -1,3 +1,4 @@
+from angr.state_plugins.sim_action import SimActionExit
 from . import Analysis
 from angr.analyses import AnalysesHub
 from angr.state_plugins.sim_action import SimActionData
@@ -22,7 +23,7 @@ class Node: #byte-level writes
 class DynamicDependenceGraph(Analysis):
     """I've changed the design. Instead of intercepting each write, read or
     branch before it occurs, allow step to run so all states are added in parallel
-    then we can ttake advantage of history and actions to construct the graph""" 
+    then we can take advantage of history and actions to construct the graph""" 
     
     #IT MIGHT BE POSSIBLE TO HAVE BOTH CONCRETE AND SYMBOLIC, i think cases would be different
     def __init__(self, project):
@@ -30,6 +31,7 @@ class DynamicDependenceGraph(Analysis):
         super().__init__(project=project)
         self.last_write_addr = defaultdict(list)
         self.id_to_node = defaultdict(list)
+        self.branch_history = defaultdict(list)
         self.node_counter = 0
         
     def _on_write(self,state, action):
@@ -56,30 +58,45 @@ class DynamicDependenceGraph(Analysis):
             for(addr, write) in self.last_write_addr.items():
                 for w in write:
                     if w.data is not None and any(v in data.variables for v in w.data.variables):
-                        self.graph.add_edge(w.id, data.id, type='DATA')
+                        self.graph.add_edge(w.id, node.id, type='DATA')
         
         
         #control dependencies
             #look at the branch history of the state and do the same thing as with data
             #tracking the branch history is the hard part
+        for cond, influencing_writes in self.branch_history:
+            if hasattr(cond, 'variables') and any(v in data.variables for v in cond.variables):
+                 for w in influencing_writes:
+                    self.graph.add_edge(w.id, node.id, type='CONTROL')
+            
             
         #potential
+        #this might kill me :) goodnight
     
+    def _check_branch(self, action):
+        # last_jmp = state.history.jumpkind
+        # if last_jmp == 'Ijk_Boring':
+        #     cond_ins = state.history.jump_source #looks like it is state.scratch.exit_ins_addr set in successsors.py
+        influencing_writes = [] #need to get the variables read
+        cond = action.condition
+        if hasattr(cond, 'variables'):
+                for (addr, writes) in self.last_write_addr.items():
+                    for w in writes:
+                        if w.data is not None and any(v in cond.variables for v in w.data.variables):
+                            influencing_writes.append(w)
+        self.branch_history.append((cond, influencing_writes))   
+        #idk what all the clone nonsense is all about MOVE FAST BREAK THINGS AHAHHAHAHAH T-T
+        
     #right now we would manually call this during our step, wed want to do it for 
     #all the states in the active stash , might be a better way to do this idk    
     def ddg_step(self, state):
+        #self._check_branch(state)
         for action in state.history.recent_actions:
             if isinstance(action, SimActionData) and action.action == 'write':
                 self._on_write(state, action)
+            if isinstance(action, SimActionExit) and action.exit_type == SimActionExit.CONDITIONAL: #im not sure this is what i think it is
+                self._check_branch(action)
                 
-    
-
-        
-    
-
-        
-    
-    
 
 AnalysesHub.register_default("DynamicDependenceGraph", DynamicDependenceGraph)
         
